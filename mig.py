b4dd525e5f6bc2ccdf9441fee5ddd1e210817f35
RasifMohammed <rasif@metrictreelabs.com>
2024-02-22 17:22:23 +0530

    expense update

--- mig.py
+++ mig.py
@@ -44,33 +44,3 @@ db_settings = {
     "db_host": os.environ.get("DATABASE_HOST"),
     "db_port": os.environ.get("DATABASE_PORT"),
 }
-db_connection = ConnectDB()
-db_connection.connect(db_settings)
-
-
-def data_migration(file_path: str, table_name: str):
-    data = []
-    with open(file_path, "r") as file:
-        csv_reader = csv.DictReader(file)
-        data = [row for row in csv_reader]
-        for i in data:
-            columns = ", ".join(i.keys())
-            placeholders = ", ".join(["%s"] * len(i))
-            values = i.values()
-            query = f"""INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"""
-            db_connection.cursor.execute(query, list(values))
-    print(f"Database values for {table_name} updated succesfully")
-    return f"Database values for {table_name} updated succesfully"
-
-
-file_path = "/home/rasif/Documents/property.csv"
-table_name = "property_property"
-data_migration(
-    file_path,
-    table_name
-)
-
-db_connection.connection.commit()
-db_connection.close_connection()
-
-print("SUCCESSFULLY DATABASE OPERATIONS COMPLETED")
--- property.py
+++ property.py
@@ -1,1 +1,35 @@
-from mig import ConnectDB
+from mig import ConnectDB,db_settings
+import csv
+
+
+db_connection = ConnectDB()
+db_connection.connect(db_settings)
+
+
+def data_migration(file_path: str, table_name: str):
+    data = []
+    with open(file_path, "r") as file:
+        csv_reader = csv.DictReader(file)
+        data = [row for row in csv_reader]
+        for i in data:
+            columns = ", ".join(i.keys())
+            print(columns,"ppppp")
+            placeholders = ", ".join(["%s"] * len(i))
+            values = i.values()
+            query = f"""INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"""
+            db_connection.cursor.execute(query, list(values))
+    print(f"Database values for {table_name} updated succesfully")
+    return f"Database values for {table_name} updated succesfully"
+
+
+file_path = "/home/rasif/Documents/property.csv"
+table_name = "property_property"
+data_migration(
+    file_path,
+    table_name
+)
+
+db_connection.connection.commit()
+db_connection.close_connection()
+
+print("SUCCESSFULLY DATABASE OPERATIONS COMPLETED")
